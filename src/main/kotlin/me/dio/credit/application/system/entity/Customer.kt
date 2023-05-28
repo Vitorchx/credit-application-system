2799cddf468b3cd7d8bb1c27f3d5a0dad2b881f2
vitorchx <vitorchx@outlook.com>
2023-05-28 18:37:18 -0300

    secund

--- src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
+++ src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -1,13 +1,18 @@
 package me.dio.credit.application.system.entity
 
-data class Customer(
-    var fistName: String = "",
-    var lastName: String = "",
-    val cpf: String,
-    var email: String = "",
-    var password: String = "",
-    var address: Address = Address(),
-    var credits: List<Credit> = mutableListOf(),
-    val id: Long? = null,
+import jakarta.persistence.*
 
-)
+@Entity
+@Table(name= "Cliente")
+data class Customer(
+    @Column(nullable = false) var fistName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false, unique = true) val cpf: String,
+    @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false) var password: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+    @Column(nullable = false) @OneToMany(fetch= FetchType.LAZY, cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST),
+        mappedBy = "customer" ) var credits: List<Credit> = mutableListOf(),
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+
+    )
